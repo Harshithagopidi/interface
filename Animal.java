8fa2a19328474e2a2abc3c4627902130f5aa0b17
Harshithagopidi <harshithareddygopidi3010@gmail.com>
2023-09-22 12:53:54 -0700

    interface commit

---
+++ Animal.java
@@ -0,0 +1,6 @@
+package OopsConcept;
+interface Animal01 {
+	  public void animalSound(); // interface method (does not have a body)
+	  public void run(); // interface method (does not have a body)
+
+}
---
+++ Example.java
@@ -0,0 +1,24 @@
+package OopsConcept;
+interface Animal {
+	  public void animalSound(); 
+	  public void sleep(); 
+	}
+
+	class Pig implements Animal {
+	  public void animalSound() {
+	    // The body of animalSound() is provided here
+	    System.out.println("The pig says: wee wee");
+	  }
+	  public void sleep() {
+	    // The body of sleep() is provided here
+	    System.out.println("Zzz");
+	  }
+	}
+
+	class Main {
+	  public static void main(String[] args) {
+	    Pig myPig = new Pig();  // Create a Pig object
+	    myPig.animalSound();
+	    myPig.sleep();
+	  }
+	}
