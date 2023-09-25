ef9d92ba962e59b43bb23848588e1f6ca445dce9
Harshithagopidi <144645322+Harshithagopidi@users.noreply.github.com>
2023-09-25 09:37:02 -0700

    Add files via upload

---
+++ MethodOverriding01.java
@@ -0,0 +1,15 @@
+package OopsConcept;
+
+public class MethodOverriding01 {
+
+	public static void main(String[] args) { 
+
+	}
+	public void setup() {
+    	System.out.println("to intialize the browser");
+    }
+    public void teradown() {
+    	System.out.println("to close the application");
+    }
+
+}
